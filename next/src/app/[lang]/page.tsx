33a22e7bf55d8ec85ffa4188bb783c4adab9b1a5
kaan <darksunnn-kaan@hotmail.com>
2025-06-05 04:43:59 +0200

    sadece deneme

--- next/src/app/[lang]/page.tsx
+++ next/src/app/[lang]/page.tsx
@@ -1,10 +1,3 @@
-import Banner from '@/components/Banner/Banner';
-import Discover from '@/components/Discover/Discover';
-import EventsPreview from '@/components/EventsPreview/EventsPreviewcopy';
-import Hero from '@/components/Hero/Hero';
-import NewsPreview from '@/components/NewsPreview/NewsPreview';
-import Partners from '@/components/Partners/Partners';
-import TelegramPreview from '@/components/TelegramPreview/TelegramPreview';
 import { getDictionary } from '@/dictionaries';
 import { getOrganizationJsonLd } from '@/libs/utils/json-ld';
 import { SupportedLanguage } from '@/models/locale';
@@ -27,15 +20,8 @@ export default async function Home(props: HomeProps) {
         id="organization-jsonld"
         type="application/ld+json"
       />
-      <Banner lang={params.lang} />
-      <EventsPreview dictionary={dictionary} lang={params.lang} />
-      <Discover dictionary={dictionary} lang={params.lang} />
-      <Hero dictionary={dictionary} lang={params.lang} />
-
-      <NewsPreview dictionary={dictionary} lang={params.lang} />
-      <TelegramPreview dictionary={dictionary} />
-      <Partners dictionary={dictionary} lang={params.lang} />
     </>
   );
 }
-//kaan
+
+// kaan
